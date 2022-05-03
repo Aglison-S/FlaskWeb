d54aa62ea7c916214792575b0f6dd694aff89990
Aglison.S <aglison.s.silva.oficial01@gmail.com>
2022-05-03 17:21:58 -0300

    Quarto Envio

--- app.py
+++ app.py
@@ -2,22 +2,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-frutas =[]
+nomes = []
+registros = []
 
 @app.route('/', methods=["GET","POST"])
 def principal():
 	# frutas = ['melão', 'melancia']
 	nome = request.form.get('nome')
 	if request.method == 'POST':
-		frutas.append(nome)
-	return render_template("index.html", frutas=frutas)
+		nomes.append(nome)
+	return render_template("index.html", nomes=nomes)
 
-@app.route('/sobre')
+@app.route('/sobre', methods=["GET","POST"])
 def sobre():
-	diario = {"fulano":6.0,
-			  "sicrano":5.0,
-			  "beltrano":4.0
-			 }
-	return render_template("sobre.html", diario=diario)
+	# diario = {"João":6.0,
+	# 		  "Paulo":5.0,
+	# 		  "José":4.0
+	# 		 }
+
+	aluno = request.form.get('aluno')
+	nota = request.form.get('nota')
+
+	if request.method == 'POST':
+		registros.append({"aluno":aluno, "nota":nota})
+	return render_template("sobre.html", registros=registros)
 
 app.run(debug=True)
